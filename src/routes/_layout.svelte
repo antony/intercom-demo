e5d4638f5aaee14ab1e6cb198abf3318883c2456
Antony Jones <aj@desirableobjects.co.uk>
2020-03-10 17:53:43 +0000

    kee

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Nav from '../components/Nav.svelte';
+  import { Messenger } from '@beyonk/svelte-intercom'
+
+  export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+
+<Messenger appId='---' />
+</main>
